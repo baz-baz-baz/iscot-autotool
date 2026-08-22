b92ad8cd090310364d002935c73dab38c530306f
AlessioBassetto <38853107+AlessioBassetto@users.noreply.github.com>
2026-08-22 20:31:46 +0200

    Claude

--- PersonalAutomationTool/obj/Debug/net10.0-windows/PersonalAutomationTool.AssemblyInfo.cs
+++ PersonalAutomationTool/obj/Debug/net10.0-windows/PersonalAutomationTool.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PersonalAutomationTool")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ca95e8b65bc12e0a5f61f343bb6579778ada3ea3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+984329746147daaee4b69ad57e3cd51731391fde")]
 [assembly: System.Reflection.AssemblyProductAttribute("PersonalAutomationTool")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PersonalAutomationTool")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
